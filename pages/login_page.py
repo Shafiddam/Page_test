099ae2a4557d155d3d2180d840edb6f30225a2ee
Shafiddam <you@example.com>
2022-02-14 12:04:09 +0300

    дозагрузим файл "login_page.py"

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert '/login' in self.browser.current_url, "LOGIN_LINK IS FALSE"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "LOGIN_FORM IS NOT PRESENTED"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "REGISTER_FORM IS NOT PRESENTED"
