077df76b170430d23ac764b7c046dd73d11ea1c9
Shafiddam <you@example.com>
2022-02-14 10:42:50 +0300

    Переходы между страницами. Есть два подхода: 1) возвращать нужный Page Object. Плюсы такого подхода: тест выглядит аккуратнее — не нужно инициализировать страницу в теле теста; явно возвращаем страницу — тип страницы ассоциирован с методом; не нужно каждый раз думать в разных тестах про инициализацию страницы — уменьшаем дублирование кода; Минусы: если у нас копится большое количество страниц и переходов — образуется много перекрестных импортов; большая связность кода — при изменении логики придется менять возвращаемое значение; сложнее понимать код, так как страница инициализируется неявно; образуются циклические зависимости, что часто приводит к ошибкам.
    Подход 2) переход происходит неявно, страницу инициализируем в теле теста.
    Плюсы:
    меньше связность кода;
    меньше импортов, нет перекрестных импортов;
    больше гибкость;
    в тесте понятнее что происходит, т.к. явно инициализируем страницу.
    Минусы:
    появляется лишний шаг в тест-кейсе;
    каждый раз при написании теста нужно думать про корректные переходы;
    дублируется код.

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from time import sleep
 from .pages.main_page import MainPage
-from pages.login_page import LoginPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,7 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     # sleep(1)                         # добавил паузу для наглядности
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -27,7 +29,7 @@ def test_guest_should_be_login_url(browser):
     page.should_be_login_url()
 
 
-def should_be_login_form():
+def test_should_be_login_form(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
@@ -35,7 +37,7 @@ def should_be_login_form():
     page.should_be_login_form()
 
 
-def should_be_register_form():
+def test_should_be_register_form(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
     page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
