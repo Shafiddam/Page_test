7a86fb9a7e1edc8d9f44b8f6d29384b6db515eaa
Shafiddam <you@example.com>
2022-02-16 17:32:41 +0300

    1) Добавили 3 отрицательные проверки (на отсутствие элемента):
    - test_guest_cant_see_success_message_after_adding_product_to_basket
    - test_guest_cant_see_success_message
    - test_message_disappeared_after_adding_product_to_basket
    
    2) соответственно на базовой странице "base_page.py" добавили 2 новых метода:
    def is_not_element_present(self, how, what, timeout=4):
    def is_disappeared(self, how, what, timeout=4):
    
    3) добавил кучу комментов для понимания кода
    
    4) добавил маркер @pytest.mark.skip на первый тест "test_guest_can_add_product_to_basket"

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,31 @@
 import math
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
+        """
+        Конструктор — метод, который вызывается, когда мы создаем объект.
+        Конструктор объявляется ключевым словом __init__.
+        В него в качестве параметров мы передаем экземпляр драйвера и url адрес.
+        Внутри конструктора сохраняем эти данные как аттрибуты нашего класса
+        """
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
 
     def is_element_present(self, how, what):
+        """
+        Метод, в котором будем перехватывать исключение. В него будем передавать два аргумента:
+        как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).
+        Чтобы перехватывать исключение, нужна конструкция try/except:
+        """
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
@@ -20,7 +33,10 @@ class BasePage():
         return True
 
     def solve_quiz_and_get_code(self):
-        # функция вычисления формулы и ввода ответа во всплывающее поле alert'a по ссылке промокода
+        """
+        вспомогательная функция вычисления формулы и ввода ответа
+        во всплывающее поле alert'a по ссылке промокода:
+        """
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
@@ -33,3 +49,27 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        """
+        абстрактный метод, который проверяет, ___ЧТО ЭЛЕМЕНТ НЕ ПОЯВЛЯЕТСЯ___
+        на странице в течение заданного времени:
+        """
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        """
+        Метод, который проверяет, ___ЧТО ЭЛЕМЕНТ ИСЧЕЗАЕТ___
+        Следует воспользоваться явным ожиданием вместе с функцией until_not,
+        в зависимости от того, какой результат мы ожидаем:
+        """
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
