980421be8f3db7dc2492fbe7149835639252425e
Shafiddam <you@example.com>
2022-02-17 16:17:54 +0300

    отсортировал методы по алфавиту

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -17,8 +18,9 @@ class BasePage():
         self.url = url
         # self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def is_element_present(self, how, what):
         """
@@ -31,24 +33,6 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
-
-    def solve_quiz_and_get_code(self):
-        """
-        вспомогательная функция вычисления формулы и ввода ответа
-        во всплывающее поле alert'a по ссылке промокода:
-        """
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
 
     def is_not_element_present(self, how, what, timeout=4):
         """
@@ -68,8 +52,32 @@ class BasePage():
         в зависимости от того, какой результат мы ожидаем:
         """
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
         return True
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def solve_quiz_and_get_code(self):
+        """
+        вспомогательная функция вычисления формулы и ввода ответа
+        во всплывающее поле alert'a по ссылке промокода:
+        """
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
