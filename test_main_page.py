967a528e36ca8429949adfca8dfdef2c8e68dda5
Shafiddam <you@example.com>
2022-02-18 15:15:16 +0300

    добавил test_guest_cant_see_product_in_basket_opened_from_main_page:     """     Гость открывает главную страницу     Переходит в корзину по кнопке в шапке сайта     Ожидаем, что в корзине нет товаров     Ожидаем, что есть текст о том что корзина пуста     """

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 from time import sleep
 from .pages.main_page import MainPage
+from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 LINK1 = 'http://selenium1py.pythonanywhere.com/'
 LINK2 = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
@@ -37,3 +39,18 @@ def test_should_be_register_form(browser):
     page = LoginPage(browser, LINK2)
     page.open()
     page.should_be_register_form()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    """
+    Гость открывает главную страницу
+    Переходит в корзину по кнопке в шапке сайта
+    Ожидаем, что в корзине нет товаров
+    Ожидаем, что есть текст о том что корзина пуста
+    """
+    link = 'http://selenium1py.pythonanywhere.com/ru'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_basket_empty()
+    page.should_be_text_basket_empty()
