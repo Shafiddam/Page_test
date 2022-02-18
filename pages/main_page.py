028d3a4f3c8a3223f1078237ab1dac9cdcfe41d8
Shafiddam <you@example.com>
2022-02-18 15:20:42 +0300

    убрал ошибку - тест не в этот файл вставил)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,4 @@
-# from selenium.webdriver.common.by import By
 from .base_page import BasePage
-# from .locators import MainPageLocators
-# from .login_page import LoginPage
 
 
 class MainPage(BasePage):
@@ -20,16 +17,8 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "LOGIN LINK IS NOT PRESENTED"
     """
+
     def __init__(self, *args, **kwargs):
-                super(MainPage, self).__init__(*args, **kwargs)
+        super(MainPage, self).__init__(*args, **kwargs)
     # или можно не городить эту конструкцию, а просто написать pass:
     # pass
-
-    def test_guest_cant_see_product_in_basket_opened_from_main_page(self):
-        """
-            Гость открывает главную страницу
-            Переходит в корзину по кнопке в шапке сайта
-            Ожидаем, что в корзине нет товаров
-            Ожидаем, что есть текст о том что корзина пуста
-        """
-
