75e20ab431faa8799a4dd28d7a62b0754c96566e
Shafiddam <you@example.com>
2022-02-18 15:23:18 +0300

    Добавил в базовую страницу def go_to_basket(self): """ Находим кнопку корзины и переходим в корзину """

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
-from .locators import BasePageLocators
+from .locators import BasePageLocators, ProductPageLocators
 
 
 class BasePage():
@@ -16,10 +16,14 @@ class BasePage():
         """
         self.browser = browser
         self.url = url
-        # self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def go_to_basket(self):
+        """ Находим кнопку корзины и переходим в корзину """
+        link = self.browser.find_element(*BasePageLocators.BASKET_BTN)
         link.click()
 
     def is_element_present(self, how, what):
@@ -36,7 +40,7 @@ class BasePage():
 
     def is_not_element_present(self, how, what, timeout=4):
         """
-        абстрактный метод, который проверяет, ___ЧТО ЭЛЕМЕНТ НЕ ПОЯВЛЯЕТСЯ___
+        Абстрактный метод, который проверяет, ___ЧТО ЭЛЕМЕНТ НЕ ПОЯВЛЯЕТСЯ___
         на странице в течение заданного времени:
         """
         try:
@@ -59,10 +63,12 @@ class BasePage():
         return True
 
     def open(self):
+        """ Открытие браузера """
         self.browser.get(self.url)
 
     def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        """ Проверка, что подстрока "login" есть в текущем url браузера """
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "LOGIN LINK IS NOT PRESENTED"
 
     def solve_quiz_and_get_code(self):
         """
@@ -77,7 +83,7 @@ class BasePage():
         try:
             alert = self.browser.switch_to.alert
             alert_text = alert.text
-            print(f"Your code: {alert_text}")
+            print(f"YOUR CODE: {alert_text}")
             alert.accept()
         except NoAlertPresentException:
-            print("No second alert presented")
+            print("NO SECOND ALERT PRESENTED")
