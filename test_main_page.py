83fde799764f310cd7a7b9e75eadf0113fb0787a
Shafiddam <you@example.com>
2022-02-21 12:05:17 +0300

    Сменил название переменной с глобальной на локальную. Косметические изменения: добавил комментарии.

--- test_main_page.py
+++ test_main_page.py
@@ -1,42 +1,50 @@
-from time import sleep
 from .pages.main_page import MainPage
 from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
-LINK1 = 'http://selenium1py.pythonanywhere.com/'
-LINK2 = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+link1 = 'http://selenium1py.pythonanywhere.com/'
+link2 = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 
 
 def test_guest_can_go_to_login_page(browser):
-    # LINK = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, LINK1)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
+    """
+    Тест, что гость может перейти на страницу логина:
+    Гость открывает главную страницу.
+    Кликает на кнопку логина.
+    """
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, LINK1)
+    """ Тест, что есть форма регистрации на странице. """
+    page = MainPage(browser, link1)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_should_be_login_url(browser):
-    page = LoginPage(browser, LINK2)
+    """ Тест, что ссылка логина корректная. """
+    page = LoginPage(browser, link2)
     page.open()
     page.should_be_login_url()
 
 
 def test_should_be_login_form(browser):
-    page = LoginPage(browser, LINK2)
+    """ Тест, что есть форма логина на странице. """
+    page = LoginPage(browser, link2)
     page.open()
     page.should_be_login_form()
 
 
 def test_should_be_register_form(browser):
-    page = LoginPage(browser, LINK2)
+    """ Тест, что есть форма регистрации на странице. """
+    page = LoginPage(browser, link2)
     page.open()
     page.should_be_register_form()
 
