ea59afb719fcc8a59340e2ea8af361f457610856
Shafiddam <you@example.com>
2022-02-21 17:54:11 +0300

    добавил новый метод """ Регистрация нового юзера """ register_new_user(self, email, password):

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,24 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        """ Регистрация нового юзера """
+        self.email = email
+        self.password = password
+        email = str(int(time.time())) + "@fakemail.org"
+        password = str(int(time.time()))
+        btn = self.browser.find_element(*LoginPageLocators.INPUT_ID_REGISTRATION_EMAIL)
+        btn.send_keys(email)
+        btn = self.browser.find_element(*LoginPageLocators.INPUT_ID_REGISTRATION_PASSWORD1)
+        btn.send_keys(password)
+        btn = self.browser.find_element(*LoginPageLocators.INPUT_ID_REGISTRATION_PASSWORD2)
+        btn.send_keys(password)
+        btn = self.browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION_SUBMIT)
+        btn.click()
+
     def should_be_login_page(self):
         """ Кажется тут прописаны "заглушки" """
         self.should_be_login_url()
