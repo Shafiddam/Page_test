722521d53c8206fe90314bb8eb368dd0ed220b15
Shafiddam <you@example.com>
2022-02-21 12:05:40 +0300

    Косметические изменения: добавил комментарии.

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,18 +4,19 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
+        """ Кажется тут прописаны "заглушки" """
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        """ Проверка на корректный url адрес. """
         assert '/login' in self.browser.current_url, "LOGIN_LINK IS FALSE"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        """ Проверка, что есть форма логина. """
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "LOGIN_FORM IS NOT PRESENTED"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        """ Проверка, что есть форма регистрации на странице. """
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "REGISTER_FORM IS NOT PRESENTED"
