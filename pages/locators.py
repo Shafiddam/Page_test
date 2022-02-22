a569fda4290c8e24408a0c5462bd93e8c821a126
Shafiddam <you@example.com>
2022-02-22 10:56:41 +0300

    добавил локаторы для регистрации нового юзера

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BTN = (By.CSS_SELECTOR, "span.btn-group")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
@@ -12,13 +13,18 @@ class BasketPageLocators():
     PRODUCTS_IN_BASKET = (By.CSS_SELECTOR, "h2.col-sm-6.h3")
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    INPUT_ID_REGISTRATION_EMAIL = (By.CSS_SELECTOR, "input#id_registration-email")
+    INPUT_ID_REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "input#id_registration-password1")
+    INPUT_ID_REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "input#id_registration-password2")
+    BUTTON_REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "button[name='registration_submit']")
+
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class ProductPageLocators():
