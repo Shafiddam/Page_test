62c289632db8c08aa836c4fd1d981085bfc97e91
Shafiddam <you@example.com>
2022-02-22 10:53:49 +0300

    - Убрал комментарии лишние

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,7 @@
 from .pages.main_page import MainPage
-from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-
-link1 = 'http://selenium1py.pythonanywhere.com/'
-link2 = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+import time
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -23,28 +20,32 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     """ Тест, что есть форма регистрации на странице. """
-    page = MainPage(browser, link1)
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_should_be_login_url(browser):
     """ Тест, что ссылка логина корректная. """
-    page = LoginPage(browser, link2)
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
 
 
 def test_should_be_login_form(browser):
     """ Тест, что есть форма логина на странице. """
-    page = LoginPage(browser, link2)
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
 
 
 def test_should_be_register_form(browser):
     """ Тест, что есть форма регистрации на странице. """
-    page = LoginPage(browser, link2)
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
 
@@ -62,3 +63,5 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     page.should_be_basket_empty()
     page.should_be_text_basket_empty()
+
+
